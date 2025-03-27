111aab8852a265cff20ba725c03a2fc86788f0f7
lukman-nov <novawiyadi@gmail.com>
2025-03-28 00:16:32 +0700

    fix: getAppMessages

--- packages/i18n/src/getAppMessages.ts
+++ packages/i18n/src/getAppMessages.ts
@@ -15,8 +15,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export async function getAppMessages({ ns, locale }: GetAppMessages): Promise<Messages> {
-  const messagesPath = path.resolve(__dirname, `../locales/${ns}/${locale}.json`);
-  const globalsPath = path.resolve(__dirname, `../locales/globals/${locale}.json`);
+  if (typeof window !== 'undefined') {
+    throw new Error('getAppMessages can only be called on the server.');
+  }
+
+  const messagesPath = path.join(__dirname, `../locales/${ns}/${locale}.json`);
+  const globalsPath = path.join(__dirname, `../locales/globals/${locale}.json`);
+
+  if (!fs.existsSync(messagesPath) || !fs.existsSync(globalsPath)) {
+    throw new Error('Message file not found.');
+  }
 
   const messages = JSON.parse(fs.readFileSync(messagesPath, 'utf-8'));
   const globalsMessages = JSON.parse(fs.readFileSync(globalsPath, 'utf-8'));
